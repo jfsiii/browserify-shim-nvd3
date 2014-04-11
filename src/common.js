6a2e9d81470ea2c4cce187d6ea5f2889f51eeca3
John Schulz <jschulz@twitter.com>
2014-04-11 11:45:36 -0700

    Show that jquery & d3 work as expected, but not nvd3

--- src/common.js
+++ src/common.js
@@ -1,5 +1,18 @@
 console.log('common.js: libs to be shared by pages');
 var d3 = require('d3');
+alert('COMMON: local d3 keys:\n\t' + Object.keys(d3).join('\n\t'));
+if (!Object.keys(d3).length) {
+  alert('COMMON: window.d3 keys:\n\t' + Object.keys(window.d3).join('\n\t'));
+}
+
 var nv = require('nvd3');
 alert('COMMON: local nv keys:\n\t' + Object.keys(nv).join('\n\t'));
-alert('COMMON: window.nv keys:\n\t' + Object.keys(window.nv).join('\n\t'));
+if (!Object.keys(nv).length) {
+  alert('COMMON: window.nv keys:\n\t' + Object.keys(window.nv).join('\n\t'));
+}
+
+var jquery = require('jquery');
+alert('COMMON: local jquery keys:\n\t' + Object.keys(jquery).join('\n\t'));
+if (!Object.keys(jquery).length) {
+  alert('COMMON: window.jQuery keys:\n\t' + Object.keys(window.jQuery).join('\n\t'));
+}
